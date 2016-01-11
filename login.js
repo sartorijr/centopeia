ce59a213c859b1ff2ad0e1851710db4f96d17d74
sartorijr <sartorijr14@gmail.com>
2016-01-11 14:27:17 -0200

    Create login.js

---
+++ login.js
@@ -0,0 +1,27 @@
+function Login(){
+	var usuario = $("#txtUsuario");
+	var senha = $("#txtSenha");
+	var erro = "input-danger";
+	
+	if (usuario.val() == 'adm' && senha.val() == 'adm'){
+		var url = "PainelAdministrativo.php";
+		$(window.document.location).attr('href',url);
+		LimparValidacao();
+		return true;
+	}else{
+		usuario.addClass(erro);
+		senha.addClass(erro);
+		return false;
+	}
+}
+
+function LimparValidacao(){
+	var usuario = $("#txtUsuario");
+	var senha = $("#txtSenha");
+	var erro = "input-danger";
+	
+	usuario.removeClass(erro);
+	senha.removeClass(erro);
+	usuario.val('');
+	senha.val('');
+}
